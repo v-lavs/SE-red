458aa8b19e2732bdf96dc78917134eb39f5f62fb
v-lavs <lavskavanda@gmail.com>
2023-05-10 23:02:53 +0300

    add style social-bar

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,21 @@
 
 
 $(document).ready(function () {
+    //HEADER SCROLL
+    let header = $('.header'),
+        scrollPrev = 0;
+
+    $(window).scroll(function () {
+        var scrolled = $(window).scrollTop();
+
+        if (scrolled > 100 && scrolled > scrollPrev) {
+            header.addClass('header_active');
+        } else {
+            header.removeClass('header_active');
+        }
+        scrollPrev = scrolled;
+    });
+
 
     // MOBILE MENU
     const nav = $('.header__nav');
