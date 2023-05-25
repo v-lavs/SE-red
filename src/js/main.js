996f7e8ed6a5d7135a9aaac5784ae77b9b5a81d3
v-lavs <lavskavanda@gmail.com>
2023-05-25 12:52:49 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -211,6 +211,22 @@ $(document).ready(function () {
         jQuery('.backdrop').fadeOut();
         $('body').removeClass('modal_open');
     })
+
+//BUTTON BACK
+    let btn = $('#btnBack');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 700) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, '300');
+    });
 });
 
 
